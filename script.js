b558dbbddcdd167a0bdedb0e85575d5bcf326f7c
Garrett Hughes <guitarguy13@ymail.com>
2018-10-30 20:25:25 -0500

    V6.3
    -Darkened Video so text pops more
    -Added React Project

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ if (document.documentElement.clientWidth < 425) {
 	let video = document.getElementById('video');
   let parent = document.getElementById('videoParent');
   parent.removeChild(video);
-  parent.style.background = "url('./mountainthumb.jpg') no-repeat center";
+  parent.style.background = "url('./mountainthumb.jpg') no-repeat center ";
+	parent.style.backgroundSize = 'cover';
   parent.style.marginBottom = '-40px';
 }
 
